93289e2d54d8149d30c94433f5dff392e277437a
Tatva Kamdar <kamdartatv79@gmail.com>
2026-01-03 08:48:25 +0530

    Added logout

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -1,10 +1,13 @@
 "use client";
 import AuthGuard from "@/components/AuthGuard";
 import Sidebar from "@/components/Sidebar";
+import { BiLogOut } from "react-icons/bi";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function DashboardLayout({ children }) {
-  const [isExpanded, setIsExpanded] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(true);
+  const router = useRouter();
   return (
     <AuthGuard>
       <div className="w-screen h-screen flex">
@@ -16,7 +19,19 @@ export default function DashboardLayout({ children }) {
           <Sidebar isExpanded={isExpanded} setIsExpanded={setIsExpanded} />
         </div>
         <div className="w-[calc(100%-260px)] flex-1 flex flex-col">
-          <div className="w-full h-20 bg-white border-b border-gray-200"></div>
+          <div className="w-full h-20 bg-white border-b border-gray-200 flex justify-end place-items-center px-8">
+            <BiLogOut
+              size={30}
+              className="text-gray-700 cursor-pointer"
+              onClick={() => {
+                document.cookie = "";
+                localStorage.removeItem("access_token");
+                localStorage.removeItem("refresh_token");
+                localStorage.removeItem("user");
+                router.push("/login");
+              }}
+            />
+          </div>
           <div className="w-full flex-1 bg-gray-50 overflow-auto">
             {children}
           </div>
